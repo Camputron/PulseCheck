9e9d6d675da7616fc16c348c02171d6db5195a5e
CamposmDev <camposm.dev@gmail.com>
2025-02-19 16:37:15 -0500

    feat(prettier): add prettier with config/ignore
    - Added `.prettierignore` to exclude unnecessary files from formatting
    - Created `prettier.config.js` for consistent code formatting
    - Updated `eslint.config.js` to integrate Prettier rules
    - Modified `package.json` and `package-lock.json` to include Prettier dependencies
    - Updated `vite.config.ts` for better development experience
    
    BREAKING CHANGE: Ensure all team members install Prettier to maintain formatting consistency.

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	trailingComma: 'none',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSameLine: true,
+	bracketSpacing: true,
+	useTabs: true,
+	semi: false,
+};
+
+export default config;
