7ce48eb348319db3ee9bca910b1ea165651b4b12
Bran7astic <budhanbrandon04@gmail.com>
2025-02-25 17:38:26 -0500

    added Features menu item, typography headers to About, FAQs, and Features components, imported those three components into the splash screen

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Container, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import About from "../components/splash/About.tsx"
+import FAQs from "../components/splash/FAQs.tsx"
+import Features from "../components/splash/Features.tsx"
 
 export default function Splash(){
 
@@ -29,11 +32,23 @@ export default function Splash(){
                  passive -- they’re part of the conversation. Instructors 
                  get instant feedback, making every session more dynamic and alive.				
                 </Typography>
-
-
-
-                
             </Box>
+            <About/>
+            <FAQs/>
+            <Features/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
 
 
         </Container>
