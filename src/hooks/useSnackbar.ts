cf5900d0ce5028792c6614ea149d39619d4d6b53
CamposmDev <camposm.dev@gmail.com>
2025-02-27 01:04:34 -0500

    feat: add useSnackbar hook
    - You may now use a hook to display snackbars instead of manually adding them to your components.

---
+++ src/hooks/useSnackbar.ts
@@ -0,0 +1,10 @@
+import { SnackbarContext } from "@/context/SnackbarContext"
+import { useContext } from "react"
+
+export default function useSnackbar() {
+  const snackbar = useContext(SnackbarContext)
+  if (!snackbar) {
+    throw new Error("useSnackbar must be used within SnackbarProvider")
+  }
+  return snackbar
+}
